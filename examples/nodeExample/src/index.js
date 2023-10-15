7246403bd28339d046a2a69bfce6fa13cbf529c7
kenilGamer <127214403+kenilGamer@users.noreply.github.com>
2023-10-15 05:54:15 +0530

    Initial commit

---
+++ examples/nodeExample/src/index.js
@@ -0,0 +1,29 @@
+import './styles.css'
+import Shery from '../.././../src/Shery'
+import * as THREE from "three"
+
+Shery.mouseFollower()
+
+Shery.hoverWithMediaCircle('#cta', {
+    images: ['https://images.unsplash.com/photo-1564865878688-9a244444042a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80']
+})
+
+
+Shery.imageMasker('#cts', {
+    mouseFollower: true,
+    text: 'Shery',
+    duration: 1
+})
+
+Shery.makeMagnet('#left-main-nav img, #left-main-nav a, #right-main-nav a')
+
+Shery.textAnimate('#left-main-nav h4', { style: 1 })
+
+Shery.imageEffect('#page-1-right', {
+    style: 2,
+    debug: true,
+    gooey: true,
+    setUniforms: (uniforms) => {
+        console.log(uniforms)
+    }
+})
